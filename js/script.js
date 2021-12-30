be3a94403abee299d957b92061f82fce3ef02711
ktrebor <robert.kedves.90@gmail.com>
2021-12-30 10:56:28 +0200

    finished the logic of the game

--- js/script.js
+++ js/script.js
@@ -2,68 +2,99 @@
 let computerScore = 0;
 let playerScore = 0;
 
-function game() {
-    //generate a random number between 0 and 2 and
-    //associate the numbers with rock = 0, paper = 1 and scissors = 2
-    function computerPlay() {
-        let randomSelection = Math.floor(Math.random() * 3);
-        if (randomSelection === 0) {
-            return 'rock';
-        } else if (randomSelection === 1) {
-            return 'paper';
+//UI
+//---obtain the input from the user 
+const weapons = document.querySelectorAll(`.button`);
+const container = document.querySelector(".container");
+const resetGame = document.querySelector("#reset-game");
+const playAgain = document.querySelector("#play-again");
+
+//---play one Round for each press of a button
+weapons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let playerSelection = button.id;
+        computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+        endGame();
+    });
+});
+
+
+
+//GAME LOGIC
+//---generate a random number between 0 and 2 and
+//---associate the numbers with rock = 0, paper = 1 and scissors = 2
+function computerPlay() {
+    let randomSelection = Math.floor(Math.random() * 3);
+    if (randomSelection === 0) {
+        return 'rock';
+    } else if (randomSelection === 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+
+//---running the rules of the game
+//--display the round result
+//---update the score
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        document.querySelector('.round-points').innerText = "It's a Tie";
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerScore++;
+        document.querySelector('.round-points').innerText = "Computer's point";
+        document.querySelector('#computer-score').innerText = computerScore;
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++;
+        document.querySelector('.round-points').innerText = "Your point";
+        document.querySelector('#player-score').innerText = playerScore;
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore++;
+        document.querySelector('.round-points').innerText = "Computer's Point";
+        document.querySelector('#computer-score').innerText = computerScore;
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++;
+        document.querySelector('.round-points').innerText = "Your point";
+        document.querySelector('#player-score').innerText = playerScore;
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerScore++;
+        document.querySelector('.round-points').innerText = "Computer's Point";
+        document.querySelector('#computer-score').innerText = computerScore;
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++;
+        document.querySelector('.round-points').innerText = "Your point";
+        document.querySelector('#player-score').innerText = playerScore;
+    }
+}
+
+//---displays the final message
+//--adds a button that resets the game
+function endGame() {
+    if (computerScore === 5 || playerScore === 5) {
+        if (computerScore > playerScore) {
+            document.querySelector('.final-results').innerHTML = `You Lost! \nFinal score is You: ${playerScore} Computer: ${computerScore}`;
         } else {
-            return 'scissors';
+            document.querySelector('.final-results').innerHTML = `You WON! \nFinal score is You: ${playerScore} Computer: ${computerScore}`;
         }
-    }
+        restartGame();
+}
+}
 
-
-    //running the rules of the game
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection === computerSelection) {
-            return "It's a tie!";
-        } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            computerScore++;
-            return "You Lose, Rock beats Scissors";
-        } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            playerScore++;
-            return "You Won, Rock beats Scissors";
-        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            computerScore++;
-            return "You Lose, Scissors beats Paper";
-        } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-            playerScore++;
-            return "You Won, Scissors beats Paper";
-        } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-            computerScore++;
-            return "You Lose, Paper beats Rock";
-        } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-            playerScore++;
-            return "You Won, Paper beats Rock";
-        }
-    }
-
-    //storing the generated value from the computer round in a variable
-    let computerSelection = computerPlay();
-    //view the generated value
-    console.log(computerSelection);
-
-    // ask the user to type rock, paper or scissors and store it into a variable string type
-    let selection = prompt('Rock, Paper or Scissors? ');
-    //convert the selection to lowercase
-    let playerSelection = selection.toLowerCase();
-    //view the input from user
-    console.log(playerSelection);
-
-    //view the results from the playround
-    console.log(playRound(playerSelection, computerSelection));
+function restartGame() {
+    container.classList.add('hidden');
+    resetGame.classList.remove('hidden');
+    playAgain.addEventListener ('click', () => {
+        container.classList.remove('hidden');
+        resetGame.classList.add('hidden');
+        computerScore = 0;
+        playerScore = 0;
+        document.querySelector('.round-points').innerText = "";
+        document.querySelector('#computer-score').innerText = computerScore;
+        document.querySelector('#player-score').innerText = playerScore;
+    })    
 }
 
 
 
-if (computerScore > playerScore) {
-    console.log(`You Lost, final score is \nYou: ${playerScore} Computer: ${computerScore} `)
-} else if (computerScore < playerScore) {
-    console.log(`You Won, final score is \nYou: ${playerScore} Computer: ${computerScore} `)
-} else {
-    console.log(`It's a TIE, final score is \nYou: ${playerScore} Computer: ${computerScore} `)
-}
